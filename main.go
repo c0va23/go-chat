0a0e006cc13be48e5895e8b04d5737b45bf4f155
Dmitrij Fedorenko <c0va23@gmail.com>
2014-12-27 17:33:56 +0300

    Handle header "Last-Event-Id".

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	
 	"github.com/toonketels/router"
 	"github.com/yosssi/ace"
+	"code.google.com/p/go-uuid/uuid"
 )
 
 func main() {
@@ -53,16 +54,24 @@ func createMessage(responseWriter http.ResponseWriter, request *http.Request) {
 	responseWriter.WriteHeader(http.StatusAccepted)
 }
 
+const LAST_EVENT_ID_HEADER = "Last-Event-ID"
+const CONTENT_TYPE_HEADER = "Content-Type"
+const EVENT_STREAM_TYPE = "text/event-stream"
+
 func getMessages(responseWriter http.ResponseWriter, request *http.Request) {
+	lastEventId := uuid.Parse(request.Header.Get(LAST_EVENT_ID_HEADER))
+
 	flusher := responseWriter.(http.Flusher)
 
-	responseWriter.Header().Add("Content-Type", "text/event-stream")
+	responseWriter.Header().Add(CONTENT_TYPE_HEADER, EVENT_STREAM_TYPE)
 	responseWriter.WriteHeader(http.StatusOK)
 
-	for message := range messageList.Iterator() {
+	for message := range messageList.Iterator(lastEventId) {
+		fmt.Fprintf(responseWriter, "id: %s\n", message.Id)
 		fmt.Fprint(responseWriter, "data: ")
 		if encodeErr := json.NewEncoder(responseWriter).Encode(message); nil != encodeErr {
-			log.Fatal(encodeErr)
+			log.Println(encodeErr)
+			return
 		}
 		fmt.Fprint(responseWriter, "\n\n")
 		flusher.Flush()
--- message.go
+++ message.go
@@ -51,21 +51,45 @@ func (messageList *MessageList) Push(message *Message) {
   messageList.Broadcast()
 }
 
-func (messageList *MessageList) Iterate(channel chan <- *Message) {
-  for index := 0; true; index++ {
-    messageList.Lock()
-    if len(messageList.Messages) == index {
-      messageList.Wait()
+type MessageIterator struct {
+  *MessageList
+  LastEventId uuid.UUID
+  Channel chan *Message
+}
+
+func (messageIterator *MessageIterator) StartIndex() int {
+  if nil != messageIterator.LastEventId {
+    messageIterator.Lock()
+    for index, message := range messageIterator.Messages {
+      if uuid.Equal(message.Id, messageIterator.LastEventId) {
+        return index
+      }
     }
-    channel <- messageList.Messages[index]
-    messageList.Unlock()
+    messageIterator.Unlock()
+  }
+  return 0
+}
+
+func (messageIterator *MessageIterator) Iterate() {
+  for index := messageIterator.StartIndex(); true; index++ {
+    messageIterator.Lock()
+    if len(messageIterator.Messages) == index {
+      messageIterator.Wait()
+    }
+    messageIterator.Channel <- messageIterator.Messages[index]
+    messageIterator.Unlock()
   }
 }
 
-func (messageList *MessageList) Iterator() <- chan *Message {
-  channel := make(chan *Message)
-  go messageList.Iterate(channel)
-  return channel
+
+func (messageList *MessageList) Iterator(lastEventId uuid.UUID) <- chan *Message {
+  messageIterator := &MessageIterator {
+    MessageList: messageList,
+    LastEventId: lastEventId,
+    Channel: make(chan *Message),
+  }
+  go messageIterator.Iterate()
+  return messageIterator.Channel
 }
 
 func (messageList *MessageList) Clean() {
